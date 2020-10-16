86528e4ad07b3b2dd7c6bad8bbf6eadf9b4ba26f
Mychal Johnson <mychalqj@gmail.com>
2020-10-15 22:25:08 -0400

    Added ability to send tweets

--- Twitter/HomeTableViewController.swift
+++ Twitter/HomeTableViewController.swift
@@ -22,6 +22,11 @@ class HomeTableViewController: UITableViewController {
         tableView.refreshControl = myRefreshControl
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        self.loadTweets()
+    }
+    
     @objc func loadTweets() {
         numberOfTweet = 20
         
